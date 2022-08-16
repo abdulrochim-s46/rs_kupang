fae808f69a0c55d55f41b56c7db69f8490a80223
Abdul Rochim <103367876+abdulrochim1910@users.noreply.github.com>
2022-08-16 21:58:18 +0700

    add files

---
+++ program.js
@@ -0,0 +1,54 @@
+// ----JavaScript for Toggle Menu---
+var navLinks = document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
+
+// ----link to media social----
+// ----PTPP----
+// var iconsPP = document.getElementById("iconsPTPP");
+function goto_internet_explorer_PTPP(){
+    location.href="https://www.ptpp.co.id/";
+}
+function goto_instagram_PTPP(){
+    location.href="https://www.instagram.com/ptpp_id/";
+}
+function goto_facebook_PTPP(){
+    location.href="https://www.facebook.com/ptpptbk/"; //https://web.facebook.com/ptpptbk?_rdc=1&_rdr
+}
+function goto_linkedin_PTPP(){
+    location.href="https://www.linkedin.com/company/ptpp/?originalSubdomain=id";
+}
+function goto_twitter_PTPP(){
+    location.href="https://twitter.com/PTPP_ID";
+}
+function goto_youtube_PTPP(){
+    location.href="https://www.youtube.com/channel/UCxkRwMjO-TJv1EdSqokmuDg"; 
+}
+
+
+// ----iconsAbdul----
+// var iconsAbdul = document.getElementById("iconsAbdul");
+function goto_instagram_abdul(){
+    location.href="https://www.instagram.com/_abdulrochim_/";
+}
+
+function goto_linkedin_abdul(){
+    location.href="https://www.linkedin.com/in/abdul-rochim-b8157150/";
+}
+
+
+// ----contact us----
+function goto_home_page(){
+    location.href="index.html";
+}
+function goto_contact_us(){
+    location.href="tel:+62-823-404-09930";
+}
+function goto_send_email(){
+    location.href="mailto: abdul.rochim.civeng@gmail.com";
+}
+
